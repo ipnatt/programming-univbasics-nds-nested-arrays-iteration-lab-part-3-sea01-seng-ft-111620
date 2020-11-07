fdf82409f897b5edd5e486938b9d2019268d6659
Irvin Natt <ipnatt@gmail.com>
2020-11-07 06:03:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,7 @@ def join_nested_strings(src)
       
       if src[row_index][element_index].is_a? String
         new_string << src[row_index][element_index]
+        return new_string.join(" ")
         element_index
       end
       element_index += 1
