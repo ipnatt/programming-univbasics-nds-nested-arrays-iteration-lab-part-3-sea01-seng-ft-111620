5949a4c415f9cbaff66f183fb0aab855f5a9612e
Irvin Natt <ipnatt@gmail.com>
2020-11-07 05:44:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def join_nested_strings(src)
   words = []
   join_nested_strings.each do |items| 
     items.each do |item|
-      if item.is_a
+      if item.is_a?(String)
         words << item
       end
     end
