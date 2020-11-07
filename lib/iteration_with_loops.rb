0462b2d3bd336ee342599b7c01abb7d41961e2e6
Irvin Natt <ipnatt@gmail.com>
2020-11-07 05:41:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,5 +2,5 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   words = []
-  join_nested_strings.each do 
+  join_nested_strings.each do |items| 
 end
